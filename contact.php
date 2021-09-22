4f8d6577be0823ce493ffcf473653363fc66ba93
52191 <52191@etu.he2b.be>
2021-09-22 20:16:13 +0200

    push

--- contact.php
+++ contact.php
@@ -1,23 +1,41 @@
 <?php
 
-    $name = trim(strip_tags($_POST['name']));
-    $email = trim(strip_tags($_POST['email']));
-    $company = trim(strip_tags($_POST['company']));
-    $message = htmlentities($_POST['message']);
+    if(isset($_POST['email']) && $_POST['email'] != '') {
+
+        if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $company = $_POST['company'];
+            $message = $_POST['message'];
 
 
-    $subject = "Contact from a visiter!";
-    $to = "olg4mirova@gmail.com";
+            $subject = "Contact from a visiter!";
+            $to = "olg4mirova@gmail.com";
+            $body = "";
 
-    $body = <<<HTML
-    $message
-    HTML;
+            $body .= "From: ".$name. "\r\n";
+            $body .= "Email: ".$email. "\r\n";
+            $body .= "Company: ".$company. "\r\n";
+            $body .= "Message: ".$message. "\r\n";
 
-    $headers = "From: $email\r\n";
-    $headers .= "Content-type: text/html\r\n";
+            
+            //mail($to, $subject, $body);
+            $message_sent = true;
+            debug_to_console("SEND OK");
 
-    mail($to, $subject, $body, $headers);
+        } else {
+            $message_sent = false;
+            debug_to_console("SEND KO");
+        }
+    }
 
-    header('Location: thanks.html');
+    function debug_to_console($data) {
+        $output = $data;
+        if (is_array($output)) {
+            $output = implode( ',', $output);
+        }
+    
+        echo "<script>console.log( 'Debug Objects: " . $output . "' );</script>";
+    }
 
 ?>
